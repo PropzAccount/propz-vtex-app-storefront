9c659957757d6839c910273c587a52eafdb8b236
Adriano.A.Santos <aadriano990@gmail.com>
2023-08-07 12:27:52 -0300

    Passado cpf do usuario para o metodo verifyPurchase

--- react/utils/verifyPurchase.ts
+++ react/utils/verifyPurchase.ts
@@ -3,39 +3,60 @@ import { formatPrice } from './formatPrice'
 interface IVerifyPurchase {
   orderForm: any
   productEan: string
+  user: string
 }
 
 export const getVerifyPurchase = ({
   orderForm,
   productEan,
+  user,
 }: IVerifyPurchase) => {
-  const itemsTickeks = orderForm.items.map(
+  const itemsTickeks = orderForm.items.reduce(
     (
-      item: { sellingPrice: number; ean: string; quantity: number },
+      acc: Array<{
+        itemId: string
+        ean: string
+        unitPrice: number
+        quantity: number
+        unitSize: string
+        blockUpdate: number
+      }>,
+
+      currentItem: {
+        sellingPrice: number
+        ean: string
+        quantity: number
+        manualPrice: number
+      },
       index: number
     ) => {
-      return {
-        itemId: String(index),
-        ean: productEan,
-        unitPrice: Number(formatPrice(item.sellingPrice)),
-        unitSize: 'Unit',
-        quantity: item.quantity,
-        blockUpdate: 0,
+      if (!currentItem.manualPrice) {
+        acc.push({
+          itemId: String(index),
+          ean: productEan,
+          unitPrice: Number(formatPrice(currentItem.sellingPrice)),
+          unitSize: 'Unit',
+          quantity: currentItem.quantity,
+          blockUpdate: 0,
+        })
       }
-    }
+
+      return acc
+    },
+    []
   )
 
   const verifyPurchase = {
     sessionId: orderForm.userProfileId,
     customer: {
-      customerId: '43012319867',
+      customerId: user,
     },
     ticket: {
       ticketId: orderForm.userProfileId,
       storeId: '3',
       posId: '1',
       employeeId: null,
-      amount: 885,
+      amount: 1,
       date: new Date(),
       blockUpdate: 0,
       items: itemsTickeks,
