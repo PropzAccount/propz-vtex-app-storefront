624a77b7b2257895f7cb68680604fe32b0daca2e
Adriano.A.Santos <43096313+AdrianoAS@users.noreply.github.com>
2024-08-02 15:32:19 -0300

    Removido busca dos produtos do hook

--- react/hooks/UseSessionAndPromotions.ts
+++ react/hooks/UseSessionAndPromotions.ts
@@ -1,39 +1,12 @@
 import { useEffect, useState } from 'react'
-import { canUseDOM } from 'vtex.render-runtime'
-import type { Product } from 'vtex.product-context/react/ProductTypes'
 
 import { getSession } from '../modules/session'
 
-interface IPromotionMaxPerCustumer {
-  pricePropz: Array<{
-    sellingPrice: number
-    listPrice: number
-  }>
-  priceVtex: Array<{
-    sellingPrice: number
-    listPrice: number
-  }>
-  maxItems: number
-  product: string
-}
-
-interface IPromotions {
-  products: Product[]
-  promotionMaxPerCustomer: IPromotionMaxPerCustumer[]
-}
-
 export const useSessionAndPromotions = () => {
-  const [promotions, setPromotions] = useState<IPromotions>({
-    products: [],
-    promotionMaxPerCustomer: [],
-  })
-
   const [session, setSession] = useState({
     isAuthenticated: false,
     user: {} as any,
   })
-
-  const [loading, setLoading] = useState(true)
 
   const sessionPromise = getSession()
 
@@ -51,43 +24,5 @@ export const useSessionAndPromotions = () => {
     })
   }, [sessionPromise])
 
-  useEffect(() => {
-    if (canUseDOM) {
-      if (session.user.namespaces?.profile?.isAuthenticated?.value === 'true') {
-        const user = session.user.namespaces.profile.document?.value
-        const controller = new AbortController()
-        const { signal } = controller
-
-        const documentUser = user?.replace(/[^0-9]+/g, '')
-
-        const getProductsPropz = async () => {
-          try {
-            const response = await fetch(
-              `/_v/get-promotion?document=${documentUser}`,
-              {
-                signal,
-              }
-            )
-
-            const dataPromotions: IPromotions = await response.json()
-
-            if (dataPromotions.products.length > 0) {
-              setPromotions(dataPromotions)
-              setLoading(false)
-              localStorage.setItem(
-                '@propz/promotion-ma-per-customer',
-                JSON.stringify(dataPromotions.promotionMaxPerCustomer)
-              )
-            }
-          } catch (error) {
-            controller.abort()
-          }
-        }
-
-        getProductsPropz()
-      }
-    }
-  }, [session])
-
-  return { session, promotions, loading }
+  return { session }
 }
